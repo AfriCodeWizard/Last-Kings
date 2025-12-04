ae28671e824e25db4d88619636163a6cd5541c72
AfriCodeWizard <AfriCodeWizard@users.noreply.github.com>
2025-12-04 09:17:02 -0800

    feat: change currency from USD to KES (Kenyan Shillings)
    - Update formatCurrency function to use en-KE locale
    - Set currency to KES with 2 decimal places
    - All monetary values now display in Kenyan Shillings

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,9 +6,11 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatCurrency(amount: number): string {
-  return new Intl.NumberFormat('en-US', {
+  return new Intl.NumberFormat('en-KE', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'KES',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
   }).format(amount);
 }
 
